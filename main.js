cb2da9623972f08e61fe08b78d5b289e09fbecfc
Kristopher Kram <kram.kristopher@gmail.com>
2021-02-14 16:50:28 +0100

    Delat in allt i functions

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-{
+getWeatherUrl = function (cityName) {
   let weatherUrl = new URL("https://api.openweathermap.org/data/2.5/weather");
 
-  weatherUrl.searchParams.set("q", "Huntingburg");
+  weatherUrl.searchParams.set("q", cityName);
   weatherUrl.searchParams.set("appid", "07666263ed3ccc84e4b232450fe32cc1");
   weatherUrl.searchParams.set("mode", "json");
   weatherUrl.searchParams.set("units", "metric");
   weatherUrl.searchParams.set("lang", "se");
 
-  document.querySelector("#weatherToday").innerText = weatherUrl;
+  return weatherUrl;
+};
+
+getWeatherInfo = function () {
+  let url = getWeatherUrl("Huntingburg"); //<< ---- SÖKRUTA
 
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", weatherUrl);
+  xhr.open("GET", url);
   xhr.responseType = "json";
 
   xhr.onload = function () {
@@ -20,11 +24,13 @@
       console.log(xhr.status + " " + xhr.statusText);
       console.log(xhr.response);
     }
-  };
-  xhr.send();
-}
 
-{
+    document.querySelector("#weatherToday").innerText = xhr.response.main.temp;
+  };
+
+  xhr.send();
+};
+getVenueUrl = function () {
   let venueUrl = new URL("https://api.foursquare.com/v2/venues/explore");
 
   venueUrl.searchParams.append(
@@ -35,12 +41,12 @@
     "client_secret",
     "TO3WZEAX252K1EPRJYPOAKWPBLJQSHH4XOJO0DOFPXEOADQT"
   );
-  venueUrl.searchParams.append("near", "Huntingburg");
+  venueUrl.searchParams.append("near", "Umeå");
   venueUrl.searchParams.append("limit", "3");
-  venueUrl.searchParams.append("v", "20210210");
-
-  document.querySelector("#venuesUrl1").innerText = venueUrl;
-
+  venueUrl.searchParams.append("v", "20210214");
+  venueUrl.searchParams.append("section", "food");
+};
+getVenueInfo = function () {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", venueUrl);
   xhr.responseType = "json";
@@ -48,12 +54,13 @@
   xhr.onload = function () {
     console.log(xhr.status + " " + xhr.statusText);
     console.log(xhr.response);
-    console.log(xhr.response.response.groups[0].items[0].venue.name);
-    console.log(xhr.response.response.groups[0].items[1].venue.name);
-    console.log(xhr.response.response.groups[0].items[2].venue.name);
 
-    //document.querySelector("venuesUrl1").innerText =
-    //xhr.response.response.groups[0].items[0].venue.name;
+    document.querySelector("#venuesUrl1").innerText =
+      xhr.response.response.groups[0].items[0].venue.name;
+    document.querySelector("#venuesUrl2").innerText =
+      xhr.response.response.groups[0].items[1].venue.name;
+    document.querySelector("#venuesUrl3").innerText =
+      xhr.response.response.groups[0].items[2].venue.name;
   };
 
   // let props = venueUrl.searchParams.entries();
@@ -61,5 +68,8 @@
   // for (const [k, v] of props) {
   //   console.log(k + ": " + v);
   // }
+
   xhr.send();
-}
+};
+
+getWeatherInfo();
